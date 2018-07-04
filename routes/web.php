6ac9162583618a2f8af77ff2a8699618e0ca3d6e
kang <38461975+taijiang01@users.noreply.github.com>
2018-07-04 15:57:47 +0800

    路由模型綁定

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/exam', 'ExamController@index')->name('exam.index');
 Route::get('/exam/create', 'ExamController@create')->name('exam.create');
 Route::post('/exam', 'ExamController@store')->name('exam.store');
 
-Route::get('/exam/{id}', 'ExamController@show')->name('exam.show');
+Route::get('/exam/{exam}', 'ExamController@show')->name('exam.show');
