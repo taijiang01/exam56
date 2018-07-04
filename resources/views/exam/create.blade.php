8505314d4f8b9259edd3bdff5d127a2d984deefa
kang <38461975+taijiang01@users.noreply.github.com>
2018-07-04 10:28:45 +0800

    建立發布測驗的表單

--- resources/views/exam/create.blade.php
+++ resources/views/exam/create.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts.app') 
 @section('content')
-    <h1>建立測驗</h1>
-@endsection
-{{ bs()->openForm('post', '/exam') }}
+    <h1>{{ __('Exam Create') }}</h1>
 
-    {{ bs()->text('username')->placeholder('請填入姓名') }}
-    {{ bs()->select('enable', ['1' => '開啟', '0' => '關閉'], '1') }}
-{{ bs()->closeForm() }}
+    @can('建立測驗')
+        {{ bs()->openForm('post', '/exam') }}
+
+            {{ bs()->formGroup()
+                ->label('測驗標題',false,'text-sm-right')
+                ->control(bs()->text('title')->placeholder('請填入測驗標題'))
+                ->showAsRow()
+                }}
+
+            {{ bs()->formGroup()
+                ->label('是否啟用？',false,'text-sm-right')
+                ->control(bs()->radioGroup('enable', [1 => '啟用', 0 => '關閉'])->selectedOption(1)->inline())
+                ->showAsRow()
+                }}
+
+            {{ bs()->submit('儲存') }}
+        {{ bs()->closeForm() }}
+    @else
+        @component('bs::alert', ['type' => 'danger'])
+            @slot('heading')
+                沒有建立測驗的權限
+            @endslot
+
+            <p>請先登入，或確定身份。</p>
+        @endcomponent
+    @endcan
+@stop
