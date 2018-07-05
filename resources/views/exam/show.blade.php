534133764797318d0676141069fb69e3149d6761
kang <38461975+taijiang01@users.noreply.github.com>
2018-07-05 08:53:03 +0800

    製作樣板

--- resources/views/exam/show.blade.php
+++ resources/views/exam/show.blade.php
@@ -1,6 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 @section('content')
-    <h1 class="text-left">{{$exam->title}}</h1>
+    <h1 class="text-center">{{$exam->title}}</h1>
 
+    <div class="text-center">
+        發佈於 {{$exam->created_at->format("Y年m月d日 H:i:s")}} / 最後更新： {{$exam->updated_at->format("Y年m月d日 H:i:s")}}
     </div>
 @endsection
