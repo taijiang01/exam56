8d0f98e205e3eea1cdf0b76989b0b77174fc95d8
kang <38461975+taijiang01@users.noreply.github.com>
2018-07-05 12:03:39 +0800

    修改測驗

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
  */
-Route::pattern('id', '[0-9]+');
+Route::pattern('exam', '[0-9]+');
 
 Route::get('/', 'ExamController@index')->name('index');
 Route::get('/home', 'ExamController@index')->name('home');
@@ -19,8 +19,9 @@ Auth::routes();
 
 Route::get('/exam', 'ExamController@index')->name('exam.index');
 Route::get('/exam/create', 'ExamController@create')->name('exam.create');
-Route::post('/exam', 'ExamController@store')->name('exam.store');
-
+Route::get('/exam/{exam}/edit', 'ExamController@edit')->name('exam.edit');
 Route::get('/exam/{exam}', 'ExamController@show')->name('exam.show');
+
+Route::post('/exam', 'ExamController@store')->name('exam.store');
 
 Route::post('/topic', 'TopicController@store')->name('topic.store');
